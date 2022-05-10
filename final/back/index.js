fd83d048e389135b741b9c0da455839f27d03820
Sirapop Mansamk <56288133+BurnRadish@users.noreply.github.com>
2022-05-10 17:27:34 +0700

    System : change port

--- final/back/index.js
+++ final/back/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT || 6000
+const port = process.env.PORT || 6556
 const { MongoClient } = require("mongodb");
 const uri = process.env.DB;
 
